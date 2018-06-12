e2677374f187b040495d02918f393e88576eee08
raghuramant <raghuramanth@gmail.com>
2018-06-12 09:30:58 +0530

    added some method as part of framework chnages

---
+++ src/framework/Framework.java
@@ -0,0 +1,14 @@
+package framework;
+
+public class Framework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		somemethod();
+
+	}
+
+	static void somemethod() {
+		System.out.println("added base class");
+	}
+}
